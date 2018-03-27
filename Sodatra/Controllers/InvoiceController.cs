726305cc5eb7ecffbaeb9263fa4e3db751df71d4
aarthi2304 <aarthi2304@gmail.com>
2018-03-27 10:08:51 +0530

    change in index.html

--- Sodatra/Controllers/InvoiceController.cs
+++ Sodatra/Controllers/InvoiceController.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using iTextSharp.text.pdf;
-using iTextSharp.text.pdf.parser;
+//using iTextSharp.text.pdf;
+//using iTextSharp.text.pdf.parser;
 using Sodatra.Content;
 
 namespace Sodatra.Controllers
